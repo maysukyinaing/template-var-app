d2db6efbac539f75e98441ec56c4c446a05d9304
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-01-29 09:58:37 +0630

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GalleryComponent} from "./gallery/gallery.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-var-app';
+
+  addNewPicture(gallery:GalleryComponent) {
+    gallery.pictures.unshift(gallery.generateImage())
+  }
+  removeFirstPicture(gallery:GalleryComponent) {
+    gallery.pictures.shift();
+  }
 }
